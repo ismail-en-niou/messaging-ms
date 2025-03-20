154891538614e207e77a5f89b8f77b6740795e8f
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2025-03-20 14:43:31 +0000

    test

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -19,9 +19,7 @@ export const ChatContextProvider = ({ children, user }) => {
   const [onlineUsers , setOnlineUsers] = useState(null);
   const [notifications , setNotification] = useState([]);
   const [allUsers , setAllusers]  = useState([]);
-  let link = "https://socket.mandomati.com.dev/";
-  // console.log("currentChat",currentChat);
-  // console.log("Notifications", notifications);
+  let link = "wss://socket.mandomati.com";
 
 
   // initial socket 
